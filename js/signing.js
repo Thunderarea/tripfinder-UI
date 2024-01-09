0152760e7bc1eae719097b9f01c794e45f8dda7b
dimleon95 <dim.leon.95@gmail.com>
2024-01-09 17:06:30 +0200

    Sign in implementation

--- js/signing.js
+++ js/signing.js
@@ -1,33 +1,36 @@
 import { showMessage } from "./message.js";
+import {postRequest} from "./api.js";
 
 // If the user is already connected, redirect to the home page
 if (localStorage.getItem("connected") === "true") window.location.href = "./index.html";
 
 document.querySelector("form#signin").addEventListener("submit", login);
 
-function login(e) {
+async function login(e) {
     e.preventDefault();
     let formData = new FormData(e.target);
 
-    let username = formData.get("username");
-    let password = formData.get("password");
-    let success = false;
-    // Here, you have to call the API and save the respective data in case of a successful connection
-    if (username === "customer" && password === "test") {
-        localStorage.setItem("role", "customer");
-        success = true;
-    } else if (username === "agency" && password === "test") {
-        localStorage.setItem("role", "agency");
-        success = true;
-    }
+    let request = {
+        "username": formData.get("username"),
+        "password": formData.get("password")
+    };
 
-    if (success) {
-        localStorage.setItem("connected", true);
-        localStorage.setItem("username", username);
-        // After a successful connection, redirect the user to the home page
-        window.location.href = "./index.html";
-    } else {
-        document.querySelector("form#signin").reset();
-        showMessage("Wrong credentials", "error");
-    }
+
+    await postRequest("auth/authenticate", request).then((res) => {
+
+        console.log(res)
+
+        if (res.data.status === "SUCCESS" ) {
+            localStorage.setItem("connected", true);
+            localStorage.setItem("username", res.data.body.username);
+            localStorage.setItem("role", res.data.body.user_type);
+            // After a successful connection, redirect the user to the home page
+            window.location.href = "./index.html";
+        } else {
+            document.querySelector("form#signin").reset();
+            showMessage(res.data.message, "error");
+        }
+    })
+
+
 }
